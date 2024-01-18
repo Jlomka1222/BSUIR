ff4c5bd1e8d21ecbde3b36b7c8154cf2dfca0469
Ilya <93597759+Jlomka1222@users.noreply.github.com>
2024-01-18 17:36:12 +0300

    add first part 5 sem

---
+++ 5 sem/lab4/cmake-build-debug/CMakeFiles/lab4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab4.dir/main.cpp.obj"
+  "lab4.exe"
+  "lab4.exe.manifest"
+  "lab4.pdb"
+  "liblab4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
